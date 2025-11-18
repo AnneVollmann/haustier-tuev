ad3693b8dbb4d20923e153666bd4b29223a17b18
Anne Vollmann <vollmann.anne@web.de>
2025-11-18 13:25:55 +0100

    feat: layout and logo

--- src/components/common/PetCard/PetCard.jsx
+++ src/components/common/PetCard/PetCard.jsx
@@ -4,10 +4,11 @@ import "./PetCard.css"
 class PetCard extends Component {
   state = {}
   render() {
-    return <div className="card" styles={{ width: '18rem' }}>
+    return <div className="pet-card card" styles={{ width: '18rem' }}>
               <img src={"/assets/img/pet-" + this.props.img + ".png"} className="card-img-top" alt="..."/>
               <div className="card-body">
                 <h5 className="card-title">{this.props.petName}</h5>
+                <button type="button" class="btn btn-add"/>
               </div>
             </div>;
   }
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -3,14 +3,17 @@ import "./HomePage.css"
 import PetCard from "../../components/common/PetCard/PetCard";
 
 class HomePage extends Component {
-    state = {  } 
-    render() { 
-        return  <div className='pets-list'>
-                    <PetCard petName="Pauli" img="1"/>
-                    <PetCard petName="Frieda" img="3"/>
-                    <PetCard petName="Elli" img="2"/>
-                </div>;
+    state = {}
+    render() {
+        return <section className="page">
+            <img className="logo" src="./assets/img/pet-weight-tracker-logo-guinea-pig.png" alt="logo"/>
+            <div className='pets-list'>
+                <PetCard petName="Pauli" img="1" />
+                <PetCard petName="Frieda" img="3" />
+                <PetCard petName="Elli" img="2" />
+            </div>
+        </section>;
     }
 }
- 
+
 export default HomePage;
